0fe770165338983dcfaf3ed04d22437a55a888c2
adlahfaqih312 <adlah.faqih.99@gmail.com>
2025-01-04 14:37:12 +0200

    Add project files.

---
+++ Recursive_Task/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Recursive_Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            void SumOfNumbers(int n)
+            {
+                int sum = 0;
+                for (int i = 0; i <= n; i++)
+                {
+                    sum = sum + i;
+                }
+                Console.WriteLine($"total sum is : {sum}");
+            }
+            Console.WriteLine("Enter the number you want : ");
+            int num = int.Parse(Console.ReadLine());
+            while (num > 0)
+            {
+                SumOfNumbers(num);
+                break;
+            }
+
+        }
+    }
+}
